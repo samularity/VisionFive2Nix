f493ef0ba5304d2e6b1f0a4ccf6ee4a458bfe7f7
samularity <samuelmunz@gmx.de>
2024-01-11 00:35:46 +0100

    split config

---
+++ visionfive2.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, nixos-hardware, ... }: {
+    imports = [
+    "${nixos-hardware}/starfive/visionfive/v2/sd-image-installer.nix"
+    ];
+
+    #use "own" kernel
+    boot.kernelPackages = (pkgs.callPackage ./linux-6.6.nix {
+    inherit (config.boot) kernelPatches;
+    });
+
+    # AND configure networking
+    networking.interfaces.end0.useDHCP = true;
+    networking.interfaces.end1.useDHCP = true;
+
+    sdImage.compressImage = true;
+
+    nixpkgs.crossSystem = {
+    config = "riscv64-unknown-linux-gnu";
+    system = "riscv64-linux";
+    };
+
+    hardware.deviceTree.overlays = [{
+    name = "8GB-patch";
+    dtsFile = "${nixos-hardware}/starfive/visionfive/v2/8gb-patch.dts";
+    }];
+}
