4a8dec2628dd3a220dd40ac95f3c25988a863e92
samularity <samuelmunz@gmx.de>
2024-01-11 00:36:42 +0100

    add common config

---
+++ common.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }: {
+
+    # set password for ssh login
+    users.users.nixos.password = "nixos";
+    
+    networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [ 23 50000 52000 ];
+        allowedUDPPorts = [ 50000 50001 ];
+        allowedUDPPortRanges = [
+            { from = 19997; to = 19999; }
+        ];
+    };
+
+    environment.systemPackages = with pkgs; [
+        wget
+        htop
+        python3
+        file
+        cmake-nix-hello-world.packages.${config.nixpkgs.crossSystem.system}.default
+        #(callPackage ./cmake/package.nix {} ) # use for loacal package
+    ];
+
+    system.stateVersion = "23.05";
+}
